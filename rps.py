95ea1cccad3c78e9419aee40597dbdb1addd16c8
Ethan12179 <ethan.cadampog@marcellin.school.nz>
2025-03-11 12:11:44 +1300

    i added a score label

--- rps.py
+++ rps.py
@@ -4,8 +4,10 @@ import random
 
 # Corrected list of choices
 things = ["rock", "paper", "scissors"]
+scoring = 0
 
 def choice(ting):
+    global scoring
     comp_choice = random.choice(things)
     if ting == comp_choice:
         tingels.config(text=f"Draw! NPC picked {comp_choice}")
@@ -13,6 +15,8 @@ def choice(ting):
          (ting == "paper" and comp_choice == "rock") or \
          (ting == "scissors" and comp_choice == "paper"):
         tingels.config(text=f"You won! NPC picked {comp_choice}")
+        scoring += 1
+        score.config(text = f"scor: {str(scoring)}")
     else:
         tingels.config(text=f"You lost! NPC picked {comp_choice}")
 
@@ -30,15 +34,18 @@ instructions.grid(column=1, row=1)
 tingels = ttk.Label(root, text='')
 tingels.grid(column=1, row=2)
 
+score = ttk.Label(root, text = 'Score : 0')
+score.grid(column=1, row=3)
+
 # Buttons
 rock = ttk.Button(root, text='Rock', command=lambda: choice("rock"))
-rock.grid(column=0, row=3)
+rock.grid(column=0, row=4)
 
 paper = ttk.Button(root, text='Paper', command=lambda: choice("paper"))
-paper.grid(column=1, row=3)
+paper.grid(column=1, row=4)
 
 scissors = ttk.Button(root, text='Scissors', command=lambda: choice("scissors"))
-scissors.grid(column=2, row=3)
+scissors.grid(column=2, row=4)
 
 # Run the Tkinter event loop
 root.mainloop()
